0d2059dccdc5a82a8fe0d1af7d770ee0fc839b3b
ferhattongel <ferhattongel@gmail.com>
2022-04-02 12:17:32 -0400

    I created arraylist

--- src/TC001/TC001.java
+++ src/TC001/TC001.java
@@ -1,12 +1,18 @@
 package TC001;
 
+import java.util.ArrayList;
+
 public class TC001 {
     public static void main(String[] args) {
         for (int i = 0; i <5 ; i++) {
             System.out.println("Ferhat");
         }
 
-
+        ArrayList<String> str=new ArrayList<>();
+        str.add("Ferhat");
+        str.add("Tongel");
+        str.add(1,"007");
+        System.out.println(str);
 
     }
 
